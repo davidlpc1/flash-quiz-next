53c8ae22b178fefef7e7351c64f6d1414194434c
Davi Lucas <davilucasdlcreator@gmail.com>
2021-01-26 18:41:43 -0300

    :sparkles: feat homepage finished

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,33 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import db from '../db.json'
+
+const theme = db.theme
 
 const GlobalStyle = createGlobalStyle`
-  body {
+ body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    font-family: 'Lato', sans-serif;
+    color: ${({ theme }) => theme.colors.contrastText};
+  }
+  html, body {
+    min-height: 100vh;
+  }
+  #__next {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
   }
 `
-
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-}
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
