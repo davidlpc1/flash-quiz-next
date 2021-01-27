ceb6306a64cd17e76d9fa2f52e35d86baf45b066
Davi Lucas <davilucasdlcreator@gmail.com>
2021-01-27 15:23:47 -0300

    :sparkles: feat button and input components finished

---
+++ src/components/Input/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const InputBase = styled.input`
+  width: 100%;
+  padding: 15px 5px;
+  font-size: 14px;
+  border: 1px solid ${({ theme }) => theme.colors.primary};
+  color: ${({ theme }) => theme.colors.contrastText};
+  background-color: ${({ theme }) => theme.colors.mainBg};
+  border-radius: ${({ theme }) => theme.borderRadius};
+  outline: 0;
+  margin-bottom: 25px;
+`;
+
+/* eslint-disable react/prop-types */
+export default function Input({
+  onChange, placeholder, maxLength, ...props
+}) {
+  return (
+    <div>
+      <InputBase
+        onChange={onChange}
+        placeholder={placeholder}
+        maxLength={maxLength}
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        {...props}
+      />
+    </div>
+  );
+}
+
+Input.defaultProps = {
+  value: '',
+};
+
+Input.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  placeholder: PropTypes.string.isRequired,
+  maxLength: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string,
+};
